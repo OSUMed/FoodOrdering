8d8534f9d65e07b1d3a31c64d6c9a3b30a9fa0af
OSUMed <62990411+OSUMed@users.noreply.github.com>
2024-05-15 21:49:05 -0700

    lesson 1: make first pizza component

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,23 @@
-import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/src/components/EditScreenInfo";
-import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import { StyleSheet, Text, View, Image } from "react-native";
+import products from "../../../assets/data/products";
 
 export default function TabOneScreen() {
+  const product = products[2];
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One Hello World yo</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
   },
   title: {
     fontSize: 20,
@@ -31,5 +27,16 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  price: {
+    fontSize: 20,
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    resizeMode: "contain",
+    marginVertical: 20,
   },
 });
